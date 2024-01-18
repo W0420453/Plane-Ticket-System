8619ac042ec45dbbd5b97c1bd9e1de7d93021203
John W <w0474402@campus.nscc.ca>
2024-01-18 15:41:27 -0400

    John commit

--- Code/Person.cpp
+++ Code/Person.cpp
@@ -2,35 +2,51 @@
 using namespace std;
 
 
-class Person {
+class Person{
     public:
-    string firstName = "";
-    string lastName = "";
-    int day = 0;
-    int month = 0;
-    int year = 0;
+    string firstName;
+    string lastName;
+    int day;
+    int month;
+    int year;
 
+    Person(){} // This is 100% required, for some reason
+    Person(bool isDefault) 
+    {
+        if (isDefault)
+        {
+            this -> firstName = "";
+            this -> lastName = ""; 
+            this -> day = 0; 
+            this -> month = 0; 
+            this -> year = 0; 
+        }
+        else 
+        {
+            GetFullName();
+            GetBirthday();
+        }
+    }
+
+    // This is a copy constructor
     void GetFullName(){
         cout << "Enter your first name: ";
         //needs input validation
         cin >> this -> firstName;
-
-
-        cout << endl <<  "Enter your last name: ";
+        cout << "Enter your last name: ";
         //needs input validation
         cin >> this -> lastName;
     }
 
+    // This is a copy constructor
     void GetBirthday(){
-
-        cout << endl << "Enter your birthday Month(mm): ";
-//       needs input validation
+        cout << "Enter your birthday Month(mm): "; 
+        //needs input validation
         cin >> this -> month;
-//        fflush(stdin);
-        cout << endl << "Enter your birthday Day(dd): ";
+        cout << "Enter your birthday Day(dd): ";
         //needs input validation
         cin >> this -> day;
-        cout << endl << "Enter your birthday Year(yyyy): ";
+        cout << "Enter your birthday Year(yyyy): ";
         //needs input validation
         cin >> this -> year;
     }
